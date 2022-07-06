e1926388008d1b1ddec1a3175475ecf3457d5ba9
Julian-G96 <j.ghiglione17@gmail.com>
2022-07-06 20:11:47 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 'HOLA';
+const nuevoNum = null;
 
 // Crea una variable booleana:
 const nuevoBool = null;
